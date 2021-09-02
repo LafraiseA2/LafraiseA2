c6a198cb2bdc238e4e1204c2b69a1cd0b7604928
LafraiseA2 <88149938+LafraiseA2@users.noreply.github.com>
2021-09-02 15:01:26 +0700

    Them mot so bai chua lam duoc

---
+++ chualam.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void debai(int a)
+{
+  Bài 708: Hãy khai báo cấu trúc dữ liệu của danh sách liên kết kép các số nguyên
+Bài 709: Hãy khai báo cấu trúc dữ liệu của cây nhị phân các số nguyên.
+Bài 712: Viết hàm tính số hạng thứ n của hai dãy sau:
+
+X(0) = 1
+Y(0) = 0
+X(n) = x(n – 1) + y(n – 1) (n > 0)
+Y(n) = 3 * x(n – 1) + 2 * y(n – 1) (n > 0)
+  
+Bài 713: Tính số hạng thứ n của dãy
+
+X(0) = 1
+X(n) = n^2.x(0) + (n – 1)^2.x(1) + … + (n – i)^2.x(i) +… + 2^2.x(n – 2) + 1^2.x(n – 1)
+}
